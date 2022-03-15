c6e771ff9e20844202870989b892dc159be9ef99
Andrew Kaplan <ac.kaplan11@berkeley.edu>
2022-03-15 07:08:31 -0700

    added schema/model for User

---
+++ schema.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const { Schema } = mongoose
+
+const userSchema = new Schema({
+    username: {
+      type: String,
+      required: true
+    }, 
+})
+
+export const User = mongoose.model('User', userSchema)
