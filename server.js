6e5720920c456f5eb0d1a22aac42a5a50e7594bc
Andrew Kaplan <ac.kaplan11@berkeley.edu>
2022-03-15 06:47:28 -0700

    connecting to mongdb client

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const mongoose = require('mongoose')
+
 require('dotenv').config()
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected"))
+  .catch((err) => console.log(err));
+
 
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
-
-
-
-
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
